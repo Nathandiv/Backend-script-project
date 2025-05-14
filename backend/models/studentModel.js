628f1612fbc58e90444a06fd51b464e2cd1f31a5
Nathandiv <k.n.hlungwani@gmail.com>
2025-05-14 14:16:46 +0200

    fix: correct formatting and add missing newlines in student model

--- backend/models/studentModel.js
+++ backend/models/studentModel.js
@@ -9,6 +9,25 @@ const studentModel = {
             }
             return callback(results);
         });
-    }
+    },
+
+    getStudentById: (id, callback) => 
+    {
+        db.query('SELECT * FROM students WHERE id = ?', [id], (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            return callback(results[0]);
+        });
+    },
+    createStudent: (student, callback) => 
+    {
+        db.query('INSERT INTO students SET ?', student, (err, results) => {
+            if (err) {
+                return callback(err);
+            }
+            return callback(results.insertId);
+        });
+    },
 
 };
